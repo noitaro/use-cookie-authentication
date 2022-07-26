4123b1f7fb31161e257ac9ffe9d32d5b2d0a1966
茄子紺 <52857466+noitaro@users.noreply.github.com>
2022-07-26 16:47:26 +0900

    init

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebApplication1.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetUser()
+        {
+            var claims = HttpContext.User.Claims;
+            var userId = claims?.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Name))?.Value;
+            return Ok(userId);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Login([FromQuery] string userId)
+        {
+            if (!userId.Equals("abc"))
+            {
+                return Unauthorized();
+            }
+
+            var claims = new List<Claim>() { new Claim(ClaimTypes.Name, userId) };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
